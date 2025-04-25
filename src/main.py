17adfabee3192ff116b27f9f4f1b5d632ac27725
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2025-04-26 02:44:06 +0900

    feat: omit column

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import re
+import json
 
 FILE_PATH=input("输入或粘贴文件路径：\n")
 
@@ -6,9 +7,10 @@ with open(FILE_PATH,"r",encoding="utf-8") as f:
     file_data=f.read()
 
 method = "datasortbody"
-stmt_payment_list=re.findall(pattern=r'data-sort=\'{.*?}\'',string=file_data)
+stmt_payment_list_data=[json.loads(line.replace("data-sort='","")[0:-1]) for line in re.findall(pattern=r'data-sort=\'{.*?}\'',string=file_data)]
 
-# print(stmt_payment_list)
+print(stmt_payment_list_data)
 
-for line in stmt_payment_list:
-    print(line.replace("data-sort='","")[0:-1])
+import pandas as pd
+stmt_payment_list=pd.DataFrame(data=[ [item["date"],item["date"],item["date"],item["date"],item["date"],item["date"] ] for item in stmt_payment_list_data],columns=["date","name","type","payment","amount","index_of_contract"])
+print(len(stmt_payment_list))
