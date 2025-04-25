f9bcb32ab3ee2d800a874aba1a2164b517cce989
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2025-04-26 03:37:02 +0900

    formatter: isort + black

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import json
 
-
 import pandas as pd
 
 FILE_PATH = input("输入或粘贴文件路径：\n")
@@ -44,19 +43,26 @@ if method == "html.datasortbody":
 
 elif method == "html.document":
     from bs4 import BeautifulSoup
-    soup=BeautifulSoup(file_data,features="lxml")
-    stmt_payment_list_data=[str(i).replace("\n","") for i in soup.find_all("div",attrs="stmt-payment-lists__i js-payment-accordion-ctrl js-payment-sort-item")]
+
+    soup = BeautifulSoup(file_data, features="lxml")
+    stmt_payment_list_data = [
+        str(i).replace("\n", "")
+        for i in soup.find_all(
+            "div",
+            attrs="stmt-payment-lists__i js-payment-accordion-ctrl js-payment-sort-item",
+        )
+    ]
     print(type(stmt_payment_list_data[0]))
     # print(stmt_payment_list[0])
     # print(len(stmt_payment_list))
     for i in stmt_payment_list_data:
-        soup_lint=BeautifulSoup(i,features="lxml")
-        soup_data=soup_lint("div",attrs="stmt-payment-lists__data")
+        soup_lint = BeautifulSoup(i, features="lxml")
+        soup_data = soup_lint("div", attrs="stmt-payment-lists__data")
         print(soup_data)
 
 elif method == "csv":
     pass
-elif method == "pdf" :
+elif method == "pdf":
     pass
 else:
     exit(0)
