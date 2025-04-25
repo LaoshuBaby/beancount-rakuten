92d79b912070e87f1124c5265f8d50695c47a0be
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2025-04-26 03:05:40 +0900

    fix: now work for html (regex method)

--- src/main.py
+++ src/main.py
@@ -1,16 +1,50 @@
-import re
 import json
-
-FILE_PATH=input("输入或粘贴文件路径：\n")
-
-with open(FILE_PATH,"r",encoding="utf-8") as f:
-    file_data=f.read()
-
-method = "datasortbody"
-stmt_payment_list_data=[json.loads(line.replace("data-sort='","")[0:-1]) for line in re.findall(pattern=r'data-sort=\'{.*?}\'',string=file_data)]
-
-print(stmt_payment_list_data)
+import re
 
 import pandas as pd
-stmt_payment_list=pd.DataFrame(data=[ [item["date"],item["date"],item["date"],item["date"],item["date"],item["date"] ] for item in stmt_payment_list_data],columns=["date","name","type","payment","amount","index_of_contract"])
-print(len(stmt_payment_list))
+
+FILE_PATH = input("输入或粘贴文件路径：\n")
+
+with open(FILE_PATH, "r", encoding="utf-8") as f:
+    file_data = f.read()
+
+method = "html.datasortbody"
+
+if method == "html.datasortbody":
+
+    stmt_payment_list = pd.DataFrame(
+        data=[
+            [
+                item["date"],
+                item["name"],
+                item["type"],
+                item["payment"],
+                item["amount"],
+                item["index_of_contract"],
+            ]
+            for item in [
+                json.loads(line.replace("data-sort='", "")[0:-1])
+                for line in re.findall(
+                    pattern=r"data-sort=\'{.*?}\'", string=file_data
+                )
+            ]
+        ],
+        columns=[
+            "date",
+            "name",
+            "type",
+            "payment",
+            "amount",
+            "index_of_contract",
+        ],
+    )
+    # print(stmt_payment_list)
+
+elif method == "html.document":
+    pass
+elif method == "csv":
+    pass
+elif method == "pdf" :
+    pass
+else:
+    exit(0)
