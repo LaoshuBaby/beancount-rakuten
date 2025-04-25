5d75c9a82f15d6207b0c5beec56f9edecddba090
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2025-04-26 02:28:20 +0900

    feat: regex, but can't get <div xxx></div>

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
-print("这就是，羁绊啊！")
+import re
+
+FILE_PATH=input("输入或粘贴文件路径：\n")
+
+with open(FILE_PATH,"r",encoding="utf-8") as f:
+    file_data=f.read()
+
+method = "datasortbody"
+stmt_payment_list=re.findall(pattern=r'data-sort=\'{.*?}\'',string=file_data)
+
+# print(stmt_payment_list)
+
+for line in stmt_payment_list:
+    print(line.replace("data-sort='","")[0:-1])
